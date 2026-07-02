4038d02b0eeecd6deda59395c5e0fa99c33d7f57
brew0day <massartcouvertures@gmail.com>
2026-07-02 04:18:40 +0200

    Mise a jour mxsend

---
+++ push_safe.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+msg="${1:-Mise a jour SENDlocal}"
+
+git add .gitignore mxsend.py email1 message.txt dkim_mail_authentifications_app_public.txt push_safe.sh
+git commit -m "$msg"
+git push -u origin main
